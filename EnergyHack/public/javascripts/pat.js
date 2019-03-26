ca82d175df00c9e3fe6c6542d5af3cadc204267c
Keshav Pandey <keshav25@outlook.com>
2019-03-26 08:26:30 -0400

    hardcode temp for api limit

--- EnergyHack/public/javascripts/pat.js
+++ EnergyHack/public/javascripts/pat.js
@@ -10,9 +10,10 @@ function getTemp(){
       }
       $.ajax(settings).done(function (response) {
         console.log(response);
-        if(document.getElementById("far").checked)
-          document.getElementById("outdoorTemperature").value = JSON.parse(response)[0]["Temperature"]["Imperial"].Value;
-        else 
-          document.getElementById("outdoorTemperature").value = JSON.parse(response)[0]["Temperature"]["Metric"].Value;
+        // if(document.getElementById("far").checked)
+        //   document.getElementById("outdoorTemperature").value = JSON.parse(response)[0]["Temperature"]["Imperial"].Value;
+        // else
+        //   document.getElementById("outdoorTemperature").value = JSON.parse(response)[0]["Temperature"]["Metric"].Value;
+        document.getElementById("outdoorTemperature").value = "26";
       });
 }
