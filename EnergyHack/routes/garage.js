aaa71e795f46bf2c1d2b2ac41bffb62516cdd41d
Vivek Dwivedi <thevivekdwivedi@gmail.com>
2019-04-04 14:36:51 -0400

    removing hardcoded weather

--- EnergyHack/routes/garage.js
+++ EnergyHack/routes/garage.js
@@ -15,22 +15,6 @@ const mongodbURI = "mongodb://dsuser:dsuserisadmin@dscluster-shard-00-00-mwrlu.m
 
 
 router.get('/', cors(), function(req, res, next) {
-    var weather;
-    var options = { method: 'GET',
-        url: 'http://dataservice.accuweather.com/currentconditions/v1/1310',
-        qs:
-            { apikey: 'o0cX0q6fUeaVKeFbacTMYf3kGEAyDILe',
-                language: 'en-us',
-                details: 'false' },
-        headers:
-            { 'Postman-Token': 'f3c8a658-2535-4ba7-a8c7-fe5a4841ced0',
-                'cache-control': 'no-cache' } };
-
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        weather = body;
-        console.log(body);
-    });
 
     MongoClient.connect(mongodbURI, { useNewUrlParser: true },function (err, client) {
         if (err) throw err;
